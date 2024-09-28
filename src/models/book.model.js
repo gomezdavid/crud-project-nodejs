accae72c550044a3b423024274518f111a9b2f36
gomezdavid <davidalejandrogomez2002@gmail.com>
2024-09-28 20:04:09 -0300

    Añado metodos restantes de la API REST, creacion de la bd con mongo, express, y docker

--- src/models/book.model.js
+++ src/models/book.model.js
@@ -1,13 +1,12 @@
-const moongose = require('mongoose')
+const mongoose = require('mongoose')
 
 const bookSchema = new mongoose.Schema(
     {
         title: String,
         author: String,
         genre: String,
-        released_date: String,
+        publication_date: String,
     }
 )
 
-//se exporta de otra forma porque es un mongomodel.
 module.exports = mongoose.model('Book', bookSchema)
